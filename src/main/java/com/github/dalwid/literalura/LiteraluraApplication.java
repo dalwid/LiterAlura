50f36fd8f8f39a677e493e575ef605fc2b1de1fa
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2024-05-21 14:06:40 -0300

    first commit

---
+++ src/main/java/com/github/dalwid/literalura/LiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.github.dalwid.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dalwid/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dalwid.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
