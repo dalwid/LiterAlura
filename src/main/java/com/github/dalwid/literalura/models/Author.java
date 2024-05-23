278c6bb0f5d7da3a529f27b5df9ec3f7825faac7
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2024-05-22 23:02:08 -0300

    trabalhndo com recods e desserealizando, e ajeitando as classes

--- src/main/java/com/github/dalwid/literalura/models/Author.java
+++ src/main/java/com/github/dalwid/literalura/models/Author.java
@@ -1,39 +1,69 @@
 package com.github.dalwid.literalura.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "authors")
 public class Author {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String name;
-    private Date birthYear, deathYear;
+    private String  name;
+    private Integer birthYear, deathYear;
 
-    private List<Book> booksAuthor;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Book> booksAuthor = new ArrayList<>();
 
     public Author() {}
 
-    public Author(String name, Date birthYear, Date deathYear, List<Book> booksAuthor) {
-        this.name = name;
-        this.birthYear = birthYear;
-        this.deathYear = deathYear;
-        this.booksAuthor = booksAuthor;
+    public Author(AuthorData data) {
+        this.name      = data.name();
+        this.birthYear = data.birthYear();
+        this.deathYear = data.deathYear();
     }
 
     public String getName() {
         return name;
     }
 
-    public Date getBirthYear() {
+    public Integer getBirthYear() {
         return birthYear;
     }
 
-    public Date getDeathYear() {
+    public Integer getDeathYear() {
         return deathYear;
     }
 
     public List<Book> getBooksAuthor() {
         return booksAuthor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBooksAuthor(List<Book> booksAuthor) {
+        booksAuthor.forEach(books -> books.setAuthor(this));
+        this.booksAuthor = booksAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "booksAuthor=" + booksAuthor +
+                ", name='" + name + '\'' +
+                ", birthYear=" + birthYear +
+                ", deathYear=" + deathYear +
+                '}';
     }
 }
--- src/main/java/com/github/dalwid/literalura/models/Book.java
+++ src/main/java/com/github/dalwid/literalura/models/Book.java
@@ -1,27 +1,67 @@
 package com.github.dalwid.literalura.models;
 
 
+import jakarta.persistence.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+@Entity
+@Table(name = "books")
 public class Book {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(unique = true)
     private String title;
 
+    private List<String> languages;
+    private Integer      downloadCount;
+
+    @ManyToOne
     private Author author;
 
     public Book(){}
 
-    public Book(String title, Author author) {
-        this.title = title;
-        this.author = author;
+    public Book(BookData data) {
+        this.title         = data.title();
+        this.languages     = data.languages();
+        this.downloadCount = data.downloadCount();
+    }
+
+    public Integer getDownloadCount() {
+        return downloadCount;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public List<String> getLanguages() {
+        return languages;
+    }
+
     public Author getAuthor() {
         return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", languages=" + languages +
+                ", downloadCount=" + downloadCount +
+                ", author=" + author.getName() +
+                '}';
     }
 }
